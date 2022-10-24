ba714884e7c5deff97c1208204b00d552606ef34
namubar <mubarakshin.nail@tatar.ru>
2022-10-24 08:57:13 +0300

    initial commit

---
+++ Example_001/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write ("Введите целое трехзначное число: ");
+int number = int.Parse (Console.ReadLine ()!);
+int newNumber (int number)
+{
+    int firstDigit =number / 100;
+    int secondDigit = (number - firstDigit * 100) / 10;
+    return secondDigit;
+}
+Console.Write (newNumber (number));
