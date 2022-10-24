219dfe5dfb9c8416523f58d4f885f35bbc2a46c2
namubar <mubarakshin.nail@tatar.ru>
2022-10-24 09:23:57 +0300

    Решена вторая задача

---
+++ Example_002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите цифру, обозначающую день недели: ");
+int weekDay = int.Parse (Console.ReadLine ()!);
+void checkDay (int weekDay)
+{
+    if (weekDay == 6 || weekDay == 7)
+    {
+        Console.WriteLine ("Этот день - выходной");
+    }
+    else
+    {
+        Console.WriteLine ("Этот день не выходной");
+    }
+}
+checkDay (weekDay);
