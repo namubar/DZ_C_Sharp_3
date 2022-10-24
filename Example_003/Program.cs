cad512c7f1aab703dc3a3f33ea6466bdf92d91a1
namubar <mubarakshin.nail@tatar.ru>
2022-10-24 14:15:09 +0300

    решена последняя задача

---
+++ Example_003/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите целое число: ");
+int number = int.Parse(Console.ReadLine()!);
+int result;
+int thirdGigit(int number)
+{
+    while ((number / 100) > 100)
+        number = number / 10;
+    result = number / 10 % 10;
+    return result;
+}
+if (number < 1000 && number > 100)
+{
+    result = number % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    if (number > 1000)
+    {
+        thirdGigit(number);
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
